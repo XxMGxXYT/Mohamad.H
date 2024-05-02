24602bd44c8bed8ef282e4801932a8cd2cf447b3
XxMGxX IT/AI <mohamad7719603@gmail.com>
2024-05-02 03:11:07 +0200

    add back top button

--- main.js
+++ main.js
@@ -2,10 +2,14 @@
 let skillsSec = document.getElementById("allSkills")
 // Get all progress span
 let allSpans = document.querySelectorAll(".prog span")
+// Get the back button
+let back = document.getElementById("back_but")
+
 
 
 // While window scrolling
 window.onscroll = function(){
+    // Progress bars
     if(window.scrollY >= skillsSec.offsetTop - 480 && window.scrollY <= skillsSec.offsetTop + 500){
         allSpans.forEach((e) => {
             e.style.width = e.dataset.width;
@@ -15,4 +19,13 @@ window.onscroll = function(){
             e.style.width = "0%";
         })
     }
+    // Progress bars
+
+    // Fly button
+    if(window.scrollY >= 696){
+        back.style.right = "10px"
+    } else{
+        back.style.right = "-60px"
+    }
+    // Fly button
 }
